691d1e4ff574d7fd86f87d02dfef38899aa88362
申鸿毓 <shenhongyu@shenhongyudeMacBook-Pro.local>
2020-01-24 14:04:53 -0600

    testing module complete?

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Producer
@@ -9,13 +11,41 @@ namespace Producer
     {
         public static void Main(string[] args)
         {
-            JObject config = new JObject();
+
+            Console.WriteLine("Please enter an absolute path to config file:\n");
+            string? filePath = Console.ReadLine();
+            JObject config = ParseConfig(filePath);
+
             ConfigToFieldsTranslator parser = new ConfigToFieldsTranslator();
             List<Field> fields = parser.Translate(config);
             RecordMaker recordMaker = new RecordMaker(fields);
             JArray record = recordMaker.MakeRecord();
 
+            Console.WriteLine("Console printing: ");
+            Console.WriteLine(record.ToString());
+            Console.WriteLine("Console exiting...");
 
+            
+
+
+        }
+
+        private static JObject ParseConfig(string? filePath)
+        {
+            Console.WriteLine($"Echoing input file path: {filePath}");
+            if (String.IsNullOrEmpty(filePath))
+            {
+                // TODO: Throw exception
+                Console.WriteLine("Got empty file path! Start trying default path...");
+                filePath = "/Users/caratan/Desktop/Spring 2020/config.json";
+            }
+            using (StreamReader stream = File.OpenText(filePath))
+            using (JsonTextReader reader = new JsonTextReader(stream))
+            {
+                // FullConfig cfg = new FullConfig(((JObject)JToken.ReadFrom(reader)));
+                return (JObject) JToken.ReadFrom(reader);
+                // return cfg;
+            }
         }
     }
 }
