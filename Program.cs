a068761bcbcc796eadef8f1425d18a042a20192a
XinchengTan <xt12@rice.edu>
2020-01-24 19:26:23 -0600

    Int, string worked; Modified configParser; Cleanup

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Producer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DoubleDataGenerator gen = new DoubleDataGenerator();
+
+            Dictionary<string, IJSONDataGeneratorFactory> dict = new Dictionary<string, IJSONDataGeneratorFactory>();
+
+            dict.Add("double", new DoubleJSONDataGeneratorFactory());
+
+
+        }
+    }
+}
